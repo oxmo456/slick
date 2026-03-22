816a1a4849b1cedfdadbaf04ab6a07c9beedc6f8
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2026-03-22 15:42:06 -0400

    chore: add basic Angular component test

---
+++ apps/slock/root/foo/foo.component.spec.ts
@@ -0,0 +1,18 @@
+import {TestBed} from '@angular/core/testing';
+import {beforeEach, describe, expect, it} from 'vitest';
+import {FooComponent} from './foo.component';
+
+describe('FooComponent', () => {
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [FooComponent],
+        }).compileComponents();
+    });
+
+    it('renders the default name', () => {
+        const fixture = TestBed.createComponent(FooComponent);
+        fixture.detectChanges();
+        const element = fixture.nativeElement.querySelector('.block div:first-child');
+        expect(element.textContent).toContain('no name');
+    });
+});
