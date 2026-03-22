0a9e5df193a0392be7139c76f41757b92cfc35cb
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2026-03-22 14:45:26 -0400

    chore: add retry for Slick WS Slock test client

--- apps/slock/root/root.component.ts
+++ apps/slock/root/root.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {retry, timer} from 'rxjs';
 import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
 
 @Component({
@@ -18,10 +19,20 @@ export class RootComponent {
             deserializer: ({data}) => data,
         });
 
-        this.socket.pipe(takeUntilDestroyed()).subscribe({
-            next: (msg) => console.log('[Slick WS]', msg),
-            error: (err) => console.error('[Slick WS] error', err),
-            complete: () => console.log('[Slick WS] disconnected'),
-        });
+        this.socket
+            .pipe(
+                retry({
+                    delay: (_, retryCount) => {
+                        console.log(`[Slick WS] reconnecting in 5s (attempt ${retryCount})`);
+                        return timer(5_000);
+                    },
+                }),
+                takeUntilDestroyed(),
+            )
+            .subscribe({
+                next: (msg) => console.log('[Slick WS]', msg),
+                error: (err) => console.error('[Slick WS] error', err),
+                complete: () => console.log('[Slick WS] disconnected'),
+            });
     }
 }
