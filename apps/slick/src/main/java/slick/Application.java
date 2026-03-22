6ddf6242d9f458210ec41638161ffdc9230a910e
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2026-03-22 10:04:07 -0400

    chore: update logs to always include pod name and pod id

--- apps/slick/src/main/java/slick/Application.java
+++ apps/slick/src/main/java/slick/Application.java
@@ -1,6 +1,7 @@
 package slick;
 
-import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -19,14 +22,7 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Pod name: " + System.getenv("POD_NAME"));
-            System.out.println("Pod UID:  " + System.getenv("POD_UID"));
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+            log.info("w00t!");
         };
     }
 }
